ca84dc9671e70188571a81c625adbe11a955747b
MatejGalic <matey.galic99@gmail.com>
2023-06-02 00:01:33 +0200

    refactor

--- SmartBikeRental/ClientApp/src/app/components/bike-lockers/bike-lockers.component.ts
+++ SmartBikeRental/ClientApp/src/app/components/bike-lockers/bike-lockers.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { merge, take, tap } from 'rxjs';
+import { BikeRentService } from 'src/app/core/services/bike-rent.service';
 import { SignalRService } from 'src/app/core/services/signal-r.service';
 
 @Component({
@@ -7,6 +9,13 @@ import { SignalRService } from 'src/app/core/services/signal-r.service';
   styleUrls: ['./bike-lockers.component.scss'],
 })
 export class BikeLockersComponent {
-  public devices$ = this.signalR.devices$;
-  constructor(private signalR: SignalRService) {}
+  public devices$ = merge(
+    this.bikeRentService.getDevices().pipe(take(1)),
+    this.signalR.devices$
+  );
+
+  constructor(
+    private signalR: SignalRService,
+    private bikeRentService: BikeRentService
+  ) {}
 }
